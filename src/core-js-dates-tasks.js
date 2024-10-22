d94e2c6832e8934b9d5acf05ca0b29cc896d6e0a
Ihar-Batura <A17331582@gmail.com>
2024-10-22 16:00:58 +0300

    feat: add solution for getCountWeekendsInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -183,8 +183,16 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let result = 0;
+  const daysInMonth = new Date(year, month, 0).getDate();
+  for (let i = 0; i < daysInMonth; i += 1) {
+    const dayOfWeek = new Date(year, month - 1, i + 1).getDay();
+    if (dayOfWeek === 0 || dayOfWeek === 6) {
+      result += 1;
+    }
+  }
+  return result;
 }
 
 /**
